17ee0edeb111aae090e581a034529ab2183afdf4
Cristi Măgherușan <cristi.magherusan@net.utcluj.ro>
2010-04-15 15:15:37 +0300

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,44 @@
+#XXX: Most of this stuff is already done in kickstart, maybe we should skip it here
+class common_base{
+	package {["ntp", "puppet", "openssh-server"]:
+		ensure => installed
+	}
+	service {["ntpd", "puppet", "sshd"]:
+		ensure => running,
+		enable => true
+	}
+	package { "epel-release":
+		provider => "rpm",
+		source => "http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm",
+		ensure => installed;
+	}
+    
+    file{"/etc/puppet/puppet.conf":
+        content => template("common-base/puppet.conf.erb"),
+        notify  => Service["puppet"]
+    }
+
+}
+define download_file(
+		$site="",
+		$local_path="", 
+		$user="") {
+
+	exec { "mkdir_for_$name":
+		command => "mkdir -p $local_path",
+		path => "/bin",
+		creates => "$local_path",
+		user => $user,
+		notify => Exec[$name]
+	}
+	exec { $name:
+		path => "/usr/bin",
+		command => "rsync -a ${site}/${name} .",
+		cwd => $local_path,
+		refreshonly => true,
+		timeout => "-1",
+		user => $user
+	}
+
+}
+
